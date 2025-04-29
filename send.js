e721c2757da59e7be0279d278fcfbe4c4838f5b1
MinsUR4 <164434993+MinsUR4@users.noreply.github.com>
2025-04-28 21:16:20 -0400

    Update send.js

--- send.js
+++ send.js
@@ -1,20 +1,3 @@
-<html>
-<head></head>
-<body>
-
-<script>
-// Set the whole page to just a button
-document.documentElement.innerHTML = `
+document.documentElement.HTML = `
     <button id="loadButton" style="font-size:24px; margin:50px;">Click to Load</button>
 `;
-
-document.getElementById('loadButton').addEventListener('click', function() {
-    document.documentElement.innerHTML = `
-        <h1 style="text-align:center; margin-top:50px;">Content Loaded!</h1>
-        <p style="text-align:center;">This appeared after you clicked the button.</p>
-    `;
-});
-</script>
-
-</body>
-</html>
